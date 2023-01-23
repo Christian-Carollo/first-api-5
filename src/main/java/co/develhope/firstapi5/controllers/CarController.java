27d502f82f9dabbc764fb2668a1236012e2081aa
Christian Carollo <carollochristian6@gmail.com>
2023-01-23 19:06:30 +0100

    Initial commit

---
+++ src/main/java/co/develhope/firstapi5/controllers/CarController.java
@@ -0,0 +1,24 @@
+package co.develhope.firstapi5.controllers;
+
+import co.develhope.firstapi5.DTO.CarDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/cars")
+public class CarController {
+
+    @GetMapping
+    public CarDTO getCar(){
+        return new CarDTO("01", "fiat", 12.000);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addCar(@RequestBody CarDTO car) {
+        System.out.println("Car ID: " + car.getId());
+        System.out.println("Car Model: " + car.getModelName());
+        System.out.println("Car Price: " + car.getPrice());
+        return new ResponseEntity<>("Auto aggiunta con successo", HttpStatus.CREATED);
+    }
+}
